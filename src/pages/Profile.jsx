1b29e60d9c96ee4d8e05d007b52fcf21d28acc0c
Andy Eka Mulya <andymulya13@gmail.com>
2023-08-08 18:44:58 +0700

    mengubah design tulisan di page profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -11,12 +11,12 @@ export default function Profile(){
                 <img src={profile} className="max-w-sm rounded-full lg:rounded-lg shadow-2xl" />
                 <h1 className="text-[200px] font-extrabold absolute top-[470px] lg:top-auto bottom-auto lg:left-auto text-cyan-950 md:text-re drop-shadow-[-13px_11px_0px_rgba(255,255,255,1)]">Hi.</h1>
                 <div>
-                    <h1 className="text-4xl font-bold uppercase bg-cyan-800 text-cyan-50">I am Andy Eka Mulya</h1>
-                    <p className="py-6 uppercase">I am a Junior Front-End Developer</p>
+                    <h1 className="text-4xl text-cyan-900">I am <span className="uppercase bg-cyan-700 text-cyan-50 font-bold">Andy Eka Mulya</span></h1>
+                    <p className="py-6 text-cyan-700">I am a <span className="uppercase font-bold text-cyan-900">Junior Front-End Developer</span></p>
                     
                     <div className='flex gap-5 lg:ml-10'>
                         <Link className="btn border-cyan-600 bg-transparent text-cyan-600" to="/portofolio">Portofolio</Link>
-                        <Modal nameContent={"Contact"} content={<Contact />} style={"btn bg-cyan-600 text-white"}/>
+                        <Modal nameContent={"Contact"} content={<Contact />} style={"btn bg-cyan-600 text-white hover:bg-cyan-700"}/>
                     </div>
                 </div>
             </div>
