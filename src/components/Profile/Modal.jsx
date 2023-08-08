dba196ff849fe2f0cba33d7d03471618c955a9a9
Andy Eka Mulya <andymulya13@gmail.com>
2023-08-08 16:23:47 +0700

    Memperbaiki responsive di page profile

--- src/components/Profile/Modal.jsx
+++ src/components/Profile/Modal.jsx
@@ -7,7 +7,6 @@ export default function Modal(){
                     <h3 className="font-bold text-lg">Hello!</h3>
                     <p className="py-4">Press ESC key or click the button below to close</p>
                     <div className="modal-action">
-                        
                         <button className="btn">Close</button>
                     </div>
                 </form>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -7,12 +7,12 @@ export default function Profile(){
         <div className="hero min-h-screen">
             <div className="hero-content flex-col lg:flex-row gap-28 lg:gap-52">
                 <img src={profile} className="max-w-sm rounded-full lg:rounded-lg shadow-2xl" />
-                <h1 className="text-[200px] font-extrabold absolute bottom-10 lg:bottom-56 lg:left-auto xl:left-auto text-cyan-950 drop-shadow-[-13px_11px_0px_rgba(255,255,255,1)]">Hi.</h1>
+                <h1 className="text-[200px] font-extrabold absolute top-[470px] lg:top-auto bottom-auto lg:left-auto text-cyan-950 md:text-re drop-shadow-[-13px_11px_0px_rgba(255,255,255,1)]">Hi.</h1>
                 <div>
                     <h1 className="text-4xl font-bold uppercase bg-cyan-800 text-cyan-50">I am Andy Eka Mulya</h1>
-                    <p className="py-6 lg:ml-10 uppercase">I am a Junior Front-End Developer</p>
+                    <p className="py-6 uppercase">I am a Junior Front-End Developer</p>
                     
-                    <div className='flex gap-5'>
+                    <div className='flex gap-5 lg:ml-10'>
                         <Link className="btn border-cyan-600 bg-transparent text-cyan-600" to="/portofolio">Portofolio</Link>
                         <Link className="btn bg-cyan-600 text-white" to="/contact">Contact</Link>
                     </div>
