a0d24acc97d5425d58cda6a8ca7355870b3517a0
Andy Eka Mulya <andymulya13@gmail.com>
2023-08-09 05:21:53 +0700

    memberikan design no-wrap pada h1 'Andy Eka Mulya' di halaman profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -8,7 +8,7 @@ export default function Profile(){
                 <img src={profile} className="max-w-sm rounded-full lg:rounded-lg shadow-2xl" />
                 <h1 className="text-[200px] font-extrabold absolute top-[470px] lg:top-auto bottom-auto lg:left-auto text-cyan-950 md:text-re drop-shadow-[-13px_11px_0px_rgba(255,255,255,1)]">Hi.</h1>
                 <div className='mt-4'>
-                    <h1 className="text-4xl text-cyan-900">I am <span className="uppercase bg-cyan-700 text-cyan-50 font-bold">Andy Eka Mulya</span></h1>
+                    <h1 className="text-4xl text-cyan-900 lg:whitespace-nowrap">I am <span className="uppercase bg-cyan-700 text-cyan-50 font-bold">Andy Eka Mulya</span></h1>
                     <p className="py-6 text-cyan-700">I am a <span className="uppercase font-bold text-cyan-900">Junior Front-End Developer</span></p>
                     
                     <div className='flex gap-5 justify-end'>
