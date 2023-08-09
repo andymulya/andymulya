d6a8fe0ca3bf5530edea74d43c101177fdf83740
Andy Eka Mulya <andymulya13@gmail.com>
2023-08-09 20:59:58 +0700

    Mengedit tampilan responsive page knowledge

--- src/pages/Knowledge.jsx
+++ src/pages/Knowledge.jsx
@@ -4,9 +4,9 @@ import Title from '../components/Title'
 export default function Knowledge(){
     return (
         <div>
-            <div className="my-10 mx-5 flex flex-col md:items-center gap-10">
+            <div className="my-10 mx-5 flex flex-col gap-10">
                 <Title title={"Knowledge"}/>
-                <div className="flex gap-5 flex-col md:flex-row my-10 md:items-center md:gap-20 md:text-center">
+                <div className="flex gap-5 flex-col my-10 md:gap-20">
                     <h1 className="text-2xl lg:text-3xl font-extrabold uppercase text-cyan-800 border-l-4 border-slate-400 pl-2 whitespace-nowrap"><span className="text-3xl md:text-5xl text-cyan-950">What</span> I Know ?</h1>
                     <div className="flex flex-col lg:flex-row flex-wrap items-center gap-5 justify-center">
                         <CardGlass title={"Javascript Programming"}/>
