6903879fb87655c5519a2cf4b0dd6f7620b80e63
Andy Eka Mulya <andymulya13@gmail.com>
2023-08-09 19:59:24 +0700

    Membuat konten dan tampilan page knowledge

--- src/pages/Knowledge.jsx
+++ src/pages/Knowledge.jsx
@@ -1,5 +1,21 @@
+import CardGlass from '../components/CardGlass'
+import Title from '../components/Title'
+
 export default function Knowledge(){
     return (
-        <h1>Knowledge</h1>
+        <div>
+            <div className="my-10 mx-5 flex flex-col md:items-center gap-10">
+                <Title title={"Knowledge"}/>
+                <div className="flex gap-5 flex-col md:flex-row my-10 md:items-center md:gap-20 md:text-center">
+                    <h1 className="text-2xl lg:text-3xl font-extrabold uppercase text-cyan-800 border-l-4 border-slate-400 pl-2 whitespace-nowrap"><span className="text-3xl md:text-5xl text-cyan-950">What</span> I Know ?</h1>
+                    <div className="flex flex-col lg:flex-row flex-wrap items-center gap-5 justify-center">
+                        <CardGlass title={"Javascript Programming"}/>
+                        <CardGlass title={"Dart Programming"} />
+                        <CardGlass title={"Vite Framework"} />
+                        <CardGlass title={"Flutter"} />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
