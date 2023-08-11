77d35f84b0a75fed28df3690ba8fbed2cc695689
Andy Eka Mulya <andymulya13@gmail.com>
2023-08-11 13:59:00 +0700

    mengubah warna tulisan pada Footer.jsx, memberikan transisi pada CustomCard.jsx, mengubah posisi pada Circle.jsx

--- src/components/CustomCard.jsx
+++ src/components/CustomCard.jsx
@@ -5,8 +5,10 @@ export default function CustomCard({ title, tag, color, shadowColor, label }){
         <div className={"card w-auto sm:w-72 h-auto bg-base-100 shadow-md border-8 border-white overflow-clip hover:shadow-xl hover:sm:scale-105 transition-all " + shadowColor}>
             <div className="card-body">
                 <Circle color={ color }/>
+
+                {/* icon */}
                 <div className="flex justify-center m-10 mb-16 z-10">
-                    <div className="w-[68px] h-[68px] bg-red-500 rounded-full border-2 border-black"/>
+                    <div className="w-[68px] h-[68px] bg-transparent rounded-full border-4 border-white"/>
                 </div>
                 <h1 className="card-title mt-5 text-sm z-10">{ title }</h1>
                 <h3 className="text-slate-600 text-xs z-10 font-semibold uppercase">{ tag }</h3>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -8,7 +8,7 @@ export default function Portfolio(){
 
             <div className="mt-10 flex flex-col sm:flex-row flex-wrap gap-10 justify-center">
                 <CustomCard title={"Simple Mobile App Al-Qur'an and Integrated With Rest API"} tag={"Flutter | Dart | API"} color={"bg-gradient-to-l from-cyan-600 to-cyan-300"} shadowColor={"shadow-cyan-600 hover:shadow-cyan-600"} label={"Flutter"}/>
-                <CustomCard title={"Simple Mobile App Cashier Bluethoot Thermal Printer"} tag={"Flutter | Dart"} color={"bg-gradient-to-l from-cyan-600 to-cyan-300"} shadowColor={"shadow-cyan-600 hover:shadow-cyan-600"} label={"Flutter"}/>
+                <CustomCard title={"Simple Mobile App Cashier Bluetooth Thermal Printer"} tag={"Flutter | Dart"} color={"bg-gradient-to-l from-cyan-600 to-cyan-300"} shadowColor={"shadow-cyan-600 hover:shadow-cyan-600"} label={"Flutter"}/>
                 <CustomCard title={"Simple Web Personal Profile Native Without Responsive - Front End"} tag={"Html | CSS | Javascript"} color={"bg-gradient-to-l from-amber-600 to-amber-400"} shadowColor={"shadow-amber-600 hover:shadow-amber-600"} label={"Javascript"}/>
                 <CustomCard title={"Simple Web Bookshelf Native Without Responsive - Front End"} tag={"Html | CSS | Javascript"} color={"bg-gradient-to-l from-amber-600 to-amber-400"} shadowColor={"shadow-amber-600 hover:shadow-amber-600"} label={"Javascript"}/>
                 <CustomCard title={"Simple Restful API Bookshelf With @Hapi - Back End"} tag={"Html | CSS | Javascript | NodeJS | @Hapi"} color={"bg-gradient-to-l from-amber-600 to-amber-400"} shadowColor={"shadow-amber-600 hover:shadow-amber-600"} label={"Javascript"}/>
