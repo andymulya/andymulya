d7475f8fad33523c11cab2c25951558faf2ff002
Andy Eka Mulya <andymulya13@gmail.com>
2023-08-11 21:15:16 +0700

    Menambahkan icon pada CustomCard di page portfolio

--- src/components/CustomCard.jsx
+++ src/components/CustomCard.jsx
@@ -1,6 +1,6 @@
 import Circle from "./Circle";
 
-export default function CustomCard({ title, tag, color, shadowColor, label }){
+export default function CustomCard({ title, tag, color, shadowColor, label, image }){
     return (
         <div className={"card w-auto sm:w-72 h-auto bg-base-100 shadow-md border-8 border-white overflow-clip hover:shadow-xl hover:sm:scale-105 transition-all " + shadowColor}>
             <div className="card-body">
@@ -8,7 +8,9 @@ export default function CustomCard({ title, tag, color, shadowColor, label }){
 
                 {/* icon */}
                 <div className="flex justify-center m-10 mb-16 z-10">
-                    <div className="w-[68px] h-[68px] bg-transparent rounded-full border-4 border-white"/>
+                    <div className="w-[64px] h-[64px]">
+                        <img src={ image }/>
+                    </div>
                 </div>
                 <h1 className="card-title mt-5 text-sm z-10">{ title }</h1>
                 <h3 className="text-slate-600 text-xs z-10 font-semibold uppercase">{ tag }</h3>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,5 +1,9 @@
 import CustomCard from '../components/CustomCard'
 import Title from '../components/Title'
+import iconQuran from '../assets/img/quran.png'
+import iconBookshelf from '../assets/img/bookshelf.png'
+import iconPrinter from '../assets/img/printer.png'
+import iconPortfolio from '../assets/img/portfolio.png'
 
 export default function Portfolio(){
     return (
@@ -7,12 +11,12 @@ export default function Portfolio(){
             <Title title={"Portfolio"}/>
 
             <div className="mt-10 flex flex-col sm:flex-row flex-wrap gap-10 justify-center">
-                <CustomCard title={"Simple Mobile App Al-Qur'an and Integrated With Rest API"} tag={"Flutter | Dart | API"} color={"bg-gradient-to-l from-cyan-600 to-cyan-300"} shadowColor={"shadow-cyan-600 hover:shadow-cyan-600"} label={"Flutter"}/>
-                <CustomCard title={"Simple Mobile App Cashier Bluetooth Thermal Printer"} tag={"Flutter | Dart"} color={"bg-gradient-to-l from-cyan-600 to-cyan-300"} shadowColor={"shadow-cyan-600 hover:shadow-cyan-600"} label={"Flutter"}/>
-                <CustomCard title={"Simple Web Personal Profile Native Without Responsive - Front End"} tag={"Html | CSS | Javascript"} color={"bg-gradient-to-l from-amber-600 to-amber-400"} shadowColor={"shadow-amber-600 hover:shadow-amber-600"} label={"Javascript"}/>
-                <CustomCard title={"Simple Web Bookshelf Native Without Responsive - Front End"} tag={"Html | CSS | Javascript"} color={"bg-gradient-to-l from-amber-600 to-amber-400"} shadowColor={"shadow-amber-600 hover:shadow-amber-600"} label={"Javascript"}/>
-                <CustomCard title={"Simple Restful API Bookshelf With @Hapi - Back End"} tag={"Html | CSS | Javascript | NodeJS | @Hapi"} color={"bg-gradient-to-l from-amber-600 to-amber-400"} shadowColor={"shadow-amber-600 hover:shadow-amber-600"} label={"Javascript"}/>
-                <CustomCard title={"Simple Web Personal Profile ReactJS Responsive - Front End"} tag={"ReactJS | Javascript | Vite | Tailwind"} color={"bg-gradient-to-l from-cyan-800 to-cyan-500"} shadowColor={"shadow-cyan-800 hover:shadow-cyan-800"} label={"ReactJS"}/>
+                <CustomCard title={"Simple Mobile App Al-Qur'an and Integrated With Rest API"} tag={"Flutter | Dart | API"} color={"bg-gradient-to-l from-cyan-600 to-cyan-300"} shadowColor={"shadow-cyan-600 hover:shadow-cyan-600"} label={"Flutter"} image={iconQuran}/>
+                <CustomCard title={"Simple Mobile App Cashier Bluetooth Thermal Printer"} tag={"Flutter | Dart"} color={"bg-gradient-to-l from-cyan-600 to-cyan-300"} shadowColor={"shadow-cyan-600 hover:shadow-cyan-600"} label={"Flutter"} image={iconPrinter}/>
+                <CustomCard title={"Simple Web Personal Profile Native Without Responsive - Front End"} tag={"Html | CSS | Javascript"} color={"bg-gradient-to-l from-amber-600 to-amber-400"} shadowColor={"shadow-amber-600 hover:shadow-amber-600"} label={"Javascript"} image={iconPortfolio}/>
+                <CustomCard title={"Simple Web Bookshelf Native Without Responsive - Front End"} tag={"Html | CSS | Javascript"} color={"bg-gradient-to-l from-amber-600 to-amber-400"} shadowColor={"shadow-amber-600 hover:shadow-amber-600"} label={"Javascript"} image={iconBookshelf}/>
+                <CustomCard title={"Simple Restful API Bookshelf With @Hapi - Back End"} tag={"Html | CSS | Javascript | NodeJS | @Hapi"} color={"bg-gradient-to-l from-amber-600 to-amber-400"} shadowColor={"shadow-amber-600 hover:shadow-amber-600"} label={"Javascript"} image={iconBookshelf}/>
+                <CustomCard title={"Simple Web Personal Profile ReactJS Responsive - Front End"} tag={"ReactJS | Javascript | Vite | Tailwind"} color={"bg-gradient-to-l from-cyan-800 to-cyan-500"} shadowColor={"shadow-cyan-800 hover:shadow-cyan-800"} label={"ReactJS"} image={iconPortfolio}/>
             </div>
         </div>
     )
