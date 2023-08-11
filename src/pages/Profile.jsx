3c57b5e087bf3760e05b1aa6d693b53438377d77
Andy Eka Mulya <andymulya13@gmail.com>
2023-08-11 18:52:00 +0700

    merubah shadow pada Hi. di page profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,7 +6,7 @@ export default function Profile(){
         <div className="hero min-h-screen">
             <div className="hero-content flex-col md:flex-row gap-24 md:gap-36 lg:gap-52">
                 <img src={profile} className="max-w-[250px] sm:max-w-sm md:max-w-xs lg:max-w-sm rounded-full shadow-2xl" />
-                <h1 className="text-[150px] sm:text-[200px] lg:text-[250px] font-extrabold absolute top-[320px] sm:top-[450px] md:top-auto bottom-auto md:left-auto text-cyan-950 drop-shadow-[-13px_11px_0px_rgba(255,255,255,1)]">Hi.</h1>
+                <h1 className="text-[150px] sm:text-[200px] lg:text-[250px] font-extrabold absolute top-[320px] sm:top-[450px] md:top-auto bottom-auto md:left-auto text-cyan-950 drop-shadow-[-12px_3px_0px_rgba(255,255,255,1)]">Hi.</h1>
                 <div className="mt-4">
                     <h2 className="flex flex-wrap flex-col sm:flex-row gap-4 text-4xl text-cyan-700">
                         <span>I am </span>
