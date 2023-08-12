59ab49ca6c359274fc0e05493fe96f8a385bc420
Andy Eka Mulya <andymulya13@gmail.com>
2023-08-13 05:38:05 +0700

    menambahkan hover:scale dicomponent infoContact.jsx di page Contact

--- src/components/contact/InfoContact.jsx
+++ src/components/contact/InfoContact.jsx
@@ -1,6 +1,6 @@
 export default function InfoContact({ icon, title, contact }){
     return (
-        <div className="flex gap-2 mt-2">
+        <div className="flex gap-2 mt-2 hover:scale-105 transition-all">
             <img src={icon} className="w-[45px] h-[45px]"/>
             <div>
                 <h1 className="uppercase font-semibold text-cyan-900">{title}</h1>
