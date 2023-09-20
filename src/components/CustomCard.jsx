45fd74c17afacf05f5d697cdb31bafa6805b381c
Andy Eka Mulya <andymulya13@gmail.com>
2023-09-20 07:50:22 +0700

    refactoring custom card di bagian portfolio

--- src/components/CustomCard.jsx
+++ src/components/CustomCard.jsx
@@ -1,19 +1,24 @@
-import Circle from "./Circle";
+/* eslint-disable react/prop-types */
+import Circle from "./Circle"
 
-export default function CustomCard({ title, tag, color, shadowColor, label, image }){
+export default function CustomCard({ title, tags, color, shadowColor, label, icon }){
     return (
-        <div className={"card w-auto sm:w-72 h-auto bg-base-100 shadow-md border-8 border-white dark:border-slate-800 overflow-clip hover:shadow-xl hover:sm:scale-105 transition-all " + shadowColor}>
+        <div className={"card w-auto sm:w-72 bg-base-100 shadow-md border-8 border-white dark:border-slate-800 overflow-clip hover:shadow-xl hover:sm:scale-105 transition-all " + shadowColor}>
             <div className="card-body">
                 <Circle color={ color }/>
 
                 {/* icon */}
                 <div className="flex justify-center m-10 mb-16 z-10">
                     <div className="w-[64px] h-[64px]">
-                        <img src={ image }/>
+                        <img src={ icon }/>
                     </div>
                 </div>
                 <h1 className="card-title mt-5 text-sm z-10 dark:text-cyan-50">{ title }</h1>
-                <h3 className="text-slate-600 dark:text-slate-400 text-xs z-10 font-semibold uppercase">{ tag }</h3>
+                <div className="flex flex-row flex-wrap gap-5">
+                    {
+                        (tags) && tags.map((tag, i) => <p key={i} className="text-xs text-slate-500 z-10 font-semibold uppercase">{ tag }</p>)
+                    }
+                </div>
                 
                 <div className="flex justify-center">
                     <label className={"text-center mt-7 w-24 py-1 rounded-full text-white uppercase text-sm font-semibold " + color}>{ label }</label>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,22 +1,85 @@
-import CustomCard from '../components/CustomCard'
 import Title from '../components/Title'
 import iconQuran from '../assets/img/quran.png'
 import iconBookshelf from '../assets/img/bookshelf.png'
 import iconPrinter from '../assets/img/printer.png'
 import iconPortfolio from '../assets/img/portfolio.png'
+import CardNativeJs from '../components/portfolio/CardNativeJs'
+import CardFlutter from '../components/portfolio/CardFlutter'
+import CardNodeJs from '../components/portfolio/CardNodeJs'
+import CardReactJs from '../components/portfolio/CardReactJs'
 
 export default function Portfolio(){
+
+    const portfolioData = [
+        {
+            title: "Simple Web E-Commerce and Integrated With Rest API",
+            tags: ["reactjs", "javascript", "api", "tailwind", "vite"],
+            label: "reactjs",
+            icon:null
+        },
+        {
+            title: "Simple Web Catatan For Save Your Catatan",
+            tags: ["reactjs", "javascript", "api", "tailwind", "vite"],
+            label: "reactjs",
+            icon:null
+        },
+        {
+            title: "Simple Mobile App Al-Qur'an and Integrated With Rest API",
+            tags:["flutter", "dart", "api"],
+            label: "flutter",
+            icon: iconQuran
+        },
+        {
+            title: "Simple Mobile App Cashier Bluetooth Thermal Printer",
+            tags: ["flutter", "dart"],
+            label: "flutter",
+            icon: iconPrinter
+        },
+        {
+            title: "Simple Web Personal Profile Native Without Responsive - Front End",
+            tags: ["html", "css", "javascript"],
+            label: "javascript",
+            icon: iconPortfolio
+        },
+        {
+            title: "Simple Web Bookshelf Native Without Responsive - Front End",
+            tags: ["html", "css", "javascript"],
+            label: "javascript",
+            icon: iconBookshelf
+        },
+        {
+            title: "Simple Restful API Bookshelf With @Hapi - Back End",
+            tags: ["javascript", "nodejs", "@hapi"],
+            label: "nodejs",
+            icon: iconBookshelf
+        },
+        {
+            title: "Simple Web Personal Profile ReactJS Responsive - Front End",
+            tags: ["reactjs", "javascript", "vite", "tailwind"],
+            label: "reactjs",
+            icon: iconPortfolio
+        }
+    ]
+
     return (
         <div className="p-10">
             <Title title={"Portfolio"}/>
 
             <div className="mt-10 flex flex-col sm:flex-row flex-wrap gap-10 justify-center">
-                <CustomCard title={"Simple Mobile App Al-Qur'an and Integrated With Rest API"} tag={"Flutter | Dart | API"} color={"bg-gradient-to-l from-cyan-600 to-cyan-300"} shadowColor={"shadow-cyan-600 hover:shadow-cyan-600"} label={"Flutter"} image={iconQuran}/>
-                <CustomCard title={"Simple Mobile App Cashier Bluetooth Thermal Printer"} tag={"Flutter | Dart"} color={"bg-gradient-to-l from-cyan-600 to-cyan-300"} shadowColor={"shadow-cyan-600 hover:shadow-cyan-600"} label={"Flutter"} image={iconPrinter}/>
-                <CustomCard title={"Simple Web Personal Profile Native Without Responsive - Front End"} tag={"Html | CSS | Javascript"} color={"bg-gradient-to-l from-amber-600 to-amber-400"} shadowColor={"shadow-amber-600 hover:shadow-amber-600"} label={"Javascript"} image={iconPortfolio}/>
-                <CustomCard title={"Simple Web Bookshelf Native Without Responsive - Front End"} tag={"Html | CSS | Javascript"} color={"bg-gradient-to-l from-amber-600 to-amber-400"} shadowColor={"shadow-amber-600 hover:shadow-amber-600"} label={"Javascript"} image={iconBookshelf}/>
-                <CustomCard title={"Simple Restful API Bookshelf With @Hapi - Back End"} tag={"Html | CSS | Javascript | NodeJS | @Hapi"} color={"bg-gradient-to-l from-amber-600 to-amber-400"} shadowColor={"shadow-amber-600 hover:shadow-amber-600"} label={"Javascript"} image={iconBookshelf}/>
-                <CustomCard title={"Simple Web Personal Profile ReactJS Responsive - Front End"} tag={"ReactJS | Javascript | Vite | Tailwind"} color={"bg-gradient-to-l from-cyan-800 to-cyan-500"} shadowColor={"shadow-cyan-800 hover:shadow-cyan-800"} label={"ReactJS"} image={iconPortfolio}/>
+
+                {
+                    portfolioData.map((data, i) => {
+                        if(data.label === "javascript"){
+                            return <CardNativeJs key={i} title={data.title} tags={data.tags} label={data.label} icon={data.icon} />
+                        }else if(data.label === "flutter"){
+                            return <CardFlutter key={i} title={data.title} tags={data.tags} label={data.label} icon={data.icon} />
+                        }else if(data.label === "nodejs"){
+                            return <CardNodeJs key={i} title={data.title} tags={data.tags} label={data.label} icon={data.icon} />
+                        }else if(data.label === "reactjs"){
+                            return <CardReactJs key={i} title={data.title} tags={data.tags} label={data.label} icon={data.icon} />
+                        }
+                    })
+                }
             </div>
         </div>
     )
